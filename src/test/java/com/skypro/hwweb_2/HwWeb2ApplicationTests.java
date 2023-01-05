d5c989c51e00421e1e7b202535a22dcc5cc1e25f
дом <amir/shakirov.2015@mail.ru>
2023-01-05 19:41:24 +0400

    Initial commit

---
+++ src/test/java/com/skypro/hwweb_2/HwWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.hwweb_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwWeb2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
